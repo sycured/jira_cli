5435e326174e22a1b950074c4f64be116c854c7f
sycured <60801403+sycured@users.noreply.github.com>
2022-09-05 09:19:32 -0500

    refacto println in license.rs

--- src/license.rs
+++ src/license.rs
@@ -9,22 +9,21 @@ use clap::{crate_description, crate_name, Command};
 
 fn license() {
     println!(
-        "{}",
-        format!(
-            "{}\n\
-            {}\n\
-            \n\
-        Copyright (c) 2022, sycured\n\
-        All rights reserved\n\
+        "{}\n\
+        {}\n\
         \n\
-        SPDX-License-Identifier: GPL-2.0-only\n\
+        Copyright (c) 2022, sycured\
+        \nAll rights reserved\
+        \n\
+        \n\
+        SPDX-License-Identifier: GPL-2.0-only\
+        \n\
         \n\
         License file:\n\
             - https://github.com/sycured/jira_cli/blob/main/LICENSE\n\
             - https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt",
-            crate_name!(),
-            crate_description!()
-        )
+        crate_name!(),
+        crate_description!()
     );
 }
 
